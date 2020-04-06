0375401274e7449bef6d81078dc340810f7f062b
Ryuya Ko <ryuyakyo25@gmail.com>
2020-04-06 18:55:30 +0900

    Add functions:set_mcond, fit

--- quantmetrics/generalized_method_of_moments.py
+++ quantmetrics/generalized_method_of_moments.py
@@ -1,32 +1,51 @@
 import numpy as np
 import pandas as pd
-import statsmodels.sandbox.regression.gmm as gmm
+import functools
+from scipy.optimize import fmin
 
 
-class gmm(gmm.GMM):
+class gmm():
     def __init__(self, data_frame, y, x, z):
         self.data_frame = data_frame
         self.y = y
         self.x = x
         self.z = z
 
-    def set_mcond(self, y, x, instrument):
-        pass
+    def set_mcond(self, mcond, param_num):
+        '''
+        setting moment conditions
+        mcond must be a function whose inputs are y, x, instrument(str), param
+        '''
+        self.mcond = mcond
+        self.param_num = param_num
 
-    def fit_qm(self, start_param=None):
+    def obj(self, param, A):
+        x = self.mcond(self.y, self.x, self.z, param)
+        return np.dot(x.T, np.dot(A, x))
+
+    def two_step_est(self):
+        self.weight = np.eye(len(self.z))
+        self.res_first = fmin(self.obj, self.start_param, (self.weight, ))
+        self.mcond_hat = self.mcond(self.y, self.x, self.z, self.res_first)
+        self.weight = np.linalg.pinv(np.dot(self.mcond_hat, self.mcond_hat.T))
+        self.res = fmin(self.obj, self.start_param, args=(self.weight, ))
+        return self.res
+
+    def fit(self, start_param=None):
         '''
         set start parameter for gmm.fit method
         '''
-        x_len = len(self.x)
         z_len = len(self.z)
-        rhs_len = x_len + z_len
+        if z_len < self.param_num:
+            raise ValueError('Num of instruments must be larger than mcond')
 
         if start_param is None:
-            self.start_param = np.random.randn(rhs_len)
+            self.start_param = np.random.randn(self.param_num).reshape([self.param_num, 1])
         else:
             self.start_param = start_param
 
-        res = self.fit(start_params=self.start_param)
+        # optimization process
+        res = self.two_step_est()
 
         return res
 
